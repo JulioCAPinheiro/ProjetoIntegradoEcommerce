186ffb9c5e63a4171d63d7dfd08433de2ffa937d
Julio <juliocesarap.df@gmail.com>
2023-11-19 01:14:32 -0300

    Corrigindo erro de login google

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -15,7 +15,15 @@ export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
+      clientSecret: process.env.GOOGLE_SECRET,
+      allowDangerousEmailAccountLinking: true,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
