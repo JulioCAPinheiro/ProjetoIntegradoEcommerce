d2e831a4a8d1431cdb32aa7d31436f990d4f7997
Julio <juliocesarap.df@gmail.com>
2023-11-21 15:20:06 -0300

    Filtro em pedidos

--- pages/orders.js
+++ pages/orders.js
@@ -1,17 +1,42 @@
 import Layout from "@/components/Layout";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import styles from '../styles/OrdersPage.module.css';
 
 export default function OrdersPage() {
     const [orders, setOrders] = useState([]);
+    const [filterPaid, setFilterPaid] = useState(null);
+
     useEffect(() => {
         axios.get('/api/orders').then(response => {
             setOrders(response.data);
         });
     }, []);
+
+    const filtedOrders = orders.filter(order => {
+        if (filterPaid !== null) {
+            return order.paid === filterPaid;
+        }
+        return true;
+    })
+
     return (
         <Layout>
             <h1>Pedidos</h1>
+
+            <div className={styles.filterContainer}>
+                <label htmlFor="filterPaid">Filtrar por Status de Pagamento:</label>
+                <select
+                    id="filterPaid"
+                    className={`${styles.select} ${filterPaid === null ? styles.all : ''}`}
+                    value={filterPaid === null ? "" : filterPaid.toString()}
+                    onChange={(e) => setFilterPaid(e.target.value === '' ? null : e.target.value === 'true')}
+                >
+                    <option value="">Todos</option>
+                    <option value="true">Pagos</option>
+                    <option value="false">Não Pagos</option>
+                </select>
+            </div>
             <table className="basic">
                 <thead>
                     <tr>
@@ -22,7 +47,7 @@ export default function OrdersPage() {
                     </tr>
                 </thead>
                 <tbody>
-                    {orders.length > 0 && orders.map(order => (
+                    {filtedOrders.length > 0 && filtedOrders.map(order => (
                         <tr key={order._id}>
                             <td>{(new Date(order.createdAt)).toLocaleString()}
                             </td>
