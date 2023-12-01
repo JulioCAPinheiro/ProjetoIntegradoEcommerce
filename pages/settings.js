1528534937cf76e78b4d65fc5a3460ccdb15dbc9
Julio <juliocesarap.df@gmail.com>
2023-12-01 00:28:21 -0300

    Removendo conteudo

--- pages/settings.js
+++
@@ -1,9 +0,0 @@
-import Layout from "@/components/Layout";
-
-export default function settings() {
-    return (
-        <Layout>
-            <h1>Estámos em trabalho aqui ! :D</h1>
-        </Layout>
-    )
-}
